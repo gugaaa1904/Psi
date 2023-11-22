7aba457abd888346e4848d16d18ce106bd5a52c7
joaorafars1 <joao57rafa@hotmail.com>
2023-11-22 02:16:42 +0000

    testing post

--- infocharge/backend/empresa.php
+++ infocharge/backend/empresa.php
@@ -2,13 +2,22 @@
 require_once 'config.php';
 $conn = connect();
 
-$sql = "INSERT INTO `empresa` (`NOME`, `ENDERECO`, `TELEFONE`, `E_MAIL`, `NUMERO_DE_FUNCIONARIOS`, `CNPJ`, `password`) 
-VALUES ('jony', 'jony', 968912443, 'joao@gmail.com', 1, 2, 'papspa')";
-
-if ($conn->query($sql) === TRUE) {
- echo "New record created successfully";
-} else {
- echo "Error: " . $sql . "<br>" . $conn->error;
+public function insert_empresa_post()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $sql = "INSERT INTO `empresa` (`NOME`, `ENDERECO`, `TELEFONE`, `E_MAIL`, `NUMERO_DE_FUNCIONARIOS`, `CNPJ`, `password`) 
+        VALUES ($this->post('NOME'), $this->post('ENDERECO'), $this->post('TELEFONE'), $this->post('E_MAIL'),$this->post('NUMERO_DE_FUNCIONARIOS'), $this->post('CNPJ'), $this->post('password'))";
+    
+        $result = $conn->query($sql); 
+        if ($result === FALSE)
+        {
+            $this->response(array('status' => 'failed'));
+        }
+        else
+        {
+            $this->response(array('status' => 'success'));
+        } 
+    }
 }
 
 ?>
