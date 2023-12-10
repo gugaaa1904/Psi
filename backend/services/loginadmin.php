7aaf07a05095bcc041b8dda3bc01803c43eae15f
Badjarda <mestredua@gmail.com>
2023-12-10 00:39:36 +0000

    mereço baby

--- backend/services/loginadmin.php
+++ backend/services/loginadmin.php
@@ -1,11 +1,12 @@
 <?php
+require_once '../config.php';
 
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-require_once '../config.php';
+
 
 class LogINAdminService
 {
