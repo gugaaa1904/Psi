3693f1883a0d37c7cb9776d81543df9cc3f142ec
gugaaa1904 <100345117+gugaaa1904@users.noreply.github.com>
2023-12-10 05:06:40 +0000

    frontend collaborador finished (falta umas coisinhas)

--- frontend/src/pages/SettingsCollaborator.js
+++ frontend/src/pages/SettingsCollaborator.js
@@ -48,10 +48,6 @@ const SettingsCollaborator = () => {
     navigate("/reports-collaborator");
   }, [navigate]);
 
-  const onTimelinesContainerClick = useCallback(() => {
-    navigate("/timeline");
-  }, [navigate]);
-
   const onDashboardContainerClick = useCallback(() => {
     navigate("/dashboard");
   }, [navigate]);
@@ -125,52 +121,37 @@ const SettingsCollaborator = () => {
       <div className={styles.settingsCollaborator}>
         <div className={styles.content}>
           <div className={styles.contentChild} />
-          <div className={styles.logout} onClick={onLogoutContainerClick}>
-            <b className={styles.button}>Log Out</b>
-          </div>
-          <div className={styles.privacy}>
-            <div className={styles.yourPrivacyIsContainer}>
-              <p
-                className={styles.yourPrivacyIs}
-              >{`Your privacy is important to us, and we want to ensure that you have control over who can access your information on this platform. Below, you can adjust your privacy settings to customize your experience. `}</p>
-              <p className={styles.yourPrivacyIs}>&nbsp;</p>
-              <p className={styles.yourPrivacyIs}>Account Security</p>
-              <p className={styles.yourPrivacyIs}>
-                Change Password: Regularly update your password to enhance the
-                security of your account.
-              </p>
-              <p className={styles.yourPrivacyIs}>&nbsp;</p>
-              <p className={styles.yourPrivacyIs}>Data Sharing</p>
-              <p className={styles.yourPrivacyIs}>
-                Information Sharing: Choose the level of information you are
-                comfortable sharing with others. [Minimal/Basic/Detailed/Custom]
-              </p>
-              <p className={styles.yourPrivacyIs}>
-                Third-Party Apps: Manage which third-party apps can access your
-                data and revoke access at any time.
-              </p>
-              <p className={styles.yourPrivacyIs}>&nbsp;</p>
-              <p className={styles.yourPrivacyIs}>Visibility</p>
-              <p className={styles.yourPrivacyIs}>
-                Profile Visibility: Control who can see your profile and your
-                posts. [Public/Friends Only/Private]
-              </p>
-              <p className={styles.yourPrivacyIs}>
-                Search Engine Indexing: Allow search engines to index your
-                profile? [Yes/No]
-              </p>
-              <p className={styles.yourPrivacyIs}>Privacy Policy</p>
-              <p className={styles.yourPrivacyIs}>
-                Review our privacy policy for a detailed understanding of how
-                your data is handled.
-              </p>
-            </div>
+          <div className={styles.accessSecurity}>
             <div className={styles.divider} />
-            <div className={styles.title}>Privacy</div>
+            <div className={styles.accessSecurity1}>{`Access & Security`}</div>
+          </div>
+
+          <div className={styles.header}>
+            <img
+              className={styles.notificationsIcon}
+              alt=""
+              src="/notifications.svg"
+              onClick={openNotifications}
+            />
+            <b className={styles.settingsheader}>Settings</b>
+          </div>
+
+          <div className={styles.language}>
+            <div className={styles.tabs}>
+              <div className={styles.tab}>
+                <div className={styles.title1}>Portuguese</div>
+              </div>
+              <div className={styles.tab1}>
+                <div className={styles.title2}>English</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.changePassword}>
+            <div className={styles.tittle}>Change your password</div>
           </div>
           <form onSubmit={handleSubmit} method="POST">
             <label>
-              <p>Old Password</p>
               <input
                 className={styles.oldPassword}
                 name="Old Password"
@@ -181,7 +162,6 @@ const SettingsCollaborator = () => {
               />
             </label>
             <label>
-              <p>New Password</p>
               <input
                 className={styles.newPassword}
                 name="New Password"
@@ -192,7 +172,6 @@ const SettingsCollaborator = () => {
               />
             </label>
             <label>
-              <p>Confirm new Password</p>
               <input
                 className={styles.confirmNewPassword}
                 name="Confirm new Password"
@@ -214,65 +193,89 @@ const SettingsCollaborator = () => {
           <div className={styles.changePassword}>
             <div className={styles.tittle}>Change your password</div>
           </div>
-          <div className={styles.language}>
-            <div className={styles.tabs}>
-              <div className={styles.tab}>
-                <div className={styles.title1}>Portuguese</div>
-              </div>
-              <div className={styles.tab1}>
-                <div className={styles.title2}>English</div>
-              </div>
-            </div>
+
+          <div
+            className={styles.changePassword}
+            onClick={() =>
+              onChangePassword(
+                document.getElementById("old_password").value,
+                document.getElementById("new_password").value,
+                document.getElementById("confirm_new_password").value
+              )
+            }
+          ></div>
+          <div className={styles.logout} onClick={onLogoutContainerClick}>
+            <b className={styles.button}>Log Out</b>
           </div>
-          <div className={styles.accessSecurity}>
-            <div className={styles.divider} />
-            <div className={styles.accessSecurity1}>{`Access & Security`}</div>
+          <div className={styles.yourPrivacyIsContainer}>
+            <p
+              className={styles.yourPrivacyIs}
+            >{`Your privacy is important to us, and we want to ensure that you have control over who can access your information on this platform. Below, you can adjust your privacy settings to customize your experience. `}</p>
+            <p className={styles.yourPrivacyIs}>&nbsp;</p>
+            <p className={styles.yourPrivacyIs}>Account Security</p>
+            <p className={styles.yourPrivacyIs}>
+              Change Password: Regularly update your password to enhance the
+              security of your account.
+            </p>
+            <p className={styles.yourPrivacyIs}>&nbsp;</p>
+            <p className={styles.yourPrivacyIs}>Data Sharing</p>
+            <p className={styles.yourPrivacyIs}>
+              Information Sharing: Choose the level of information you are
+              comfortable sharing with others. [Minimal/Basic/Detailed/Custom]
+            </p>
+            <p className={styles.yourPrivacyIs}>
+              Third-Party Apps: Manage which third-party apps can access your
+              data and revoke access at any time.
+            </p>
+            <p className={styles.yourPrivacyIs}>&nbsp;</p>
+            <p className={styles.yourPrivacyIs}>Visibility</p>
+            <p className={styles.yourPrivacyIs}>
+              Profile Visibility: Control who can see your profile and your
+              posts. [Public/Friends Only/Private]
+            </p>
+            <p className={styles.yourPrivacyIs}>
+              Search Engine Indexing: Allow search engines to index your
+              profile? [Yes/No]
+            </p>
+            <p className={styles.yourPrivacyIs}>Privacy Policy</p>
+            <p className={styles.yourPrivacyIs}>
+              Review our privacy policy for a detailed understanding of how your
+              data is handled.
+            </p>
           </div>
+
+          <div className={styles.title}>Privacy</div>
+          <div className={styles.divider} />
+          <div className={styles.divider1} />
+          <div className={styles.divider1} />
         </div>
-        <div className={styles.header}>
-          <img
-            className={styles.notificationsIcon}
-            alt=""
-            src="/notifications.svg"
-            onClick={openNotifications}
-          />
-          <b className={styles.settings}>Settings</b>
-        </div>
+
         <div className={styles.sidebar}>
-          <div className={styles.settings1}>
-            <img className={styles.settingsIcon} alt="" src="/settings1.svg" />
-            <b className={styles.settings2}>Settings</b>
-          </div>
-          <div className={styles.help} onClick={onHelpContainerClick}>
-            <img className={styles.iconshelp} alt="" src="/iconshelp.svg" />
-            <div className={styles.help1}>Help</div>
-          </div>
           <div className={styles.menu}>
-            <div className={styles.profile} onClick={onProfileContainerClick}>
-              <div className={styles.profile1}>Profile</div>
-            </div>
-            <div className={styles.reports} onClick={onReportsContainerClick}>
-              <div className={styles.reportsTexto}>Reports</div>
-            </div>
-            <div
-              className={styles.timelines}
-              onClick={onTimelinesContainerClick}
-            >
-              <div className={styles.profile1}>Timelines</div>
-            </div>
+            <b className={styles.menu1}>MENU</b>
             <div
               className={styles.dashboard}
               onClick={onDashboardContainerClick}
             >
-              <div className={styles.profile1}>Dashboard</div>
+              <div className={styles.dashboard1}>Dashboard</div>
             </div>
-            <b className={styles.menu1}>MENU</b>
+            <div className={styles.reports} onClick={onReportsContainerClick}>
+              <div className={styles.reportsTexto}>Reports</div>
+            </div>
+
+            <div className={styles.profile} onClick={onProfileContainerClick}>
+              <div className={styles.profile1}>Profile</div>
+            </div>
           </div>
+          <div className={styles.help5} onClick={onHelpContainerClick}>
+            <b className={styles.help6}>Help</b>
+          </div>
+          <div className={styles.settings}>
+            <div className={styles.settings1}>Settings</div>
+          </div>
+
+          <img className={styles.logo1Icon} alt="" src="/logoinfocharge.png" />
           <div className={styles.line} />
-          <div className={styles.line1} />
-          <div className={styles.line2} />
-          <div className={styles.line3} />
-          <img className={styles.logo1Icon} alt="" src="/logo-11@2x.png" />
         </div>
       </div>
       {isPopUpChangePasswordOpen && (
